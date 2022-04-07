c4c79c88b0867c7523c9e961800e7378e99d61a1
Bimesh13 <bimesh.shrestha13@gmail.com>
2022-04-07 18:35:22 +0545

    Random image generated using useState.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,8 +1,25 @@
 import React from "react";
+import memesData from "../data.js";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function generateImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomIndex = Math.floor(Math.random() * memesArray.length);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: memesArray[randomIndex].url,
+    }));
+  }
   return (
-    <form>
+    <div className="form">
       <div className="input--fields">
         <input
           type="text"
@@ -15,7 +32,10 @@ export default function Meme() {
           className="form--input"
         />
       </div>
-      <button className="generate--button">Get a new meme image 🖼</button>
-    </form>
+      <button onClick={generateImage} className="generate--button">
+        Get a new meme image 🖼
+      </button>
+      <img src={meme.randomImage} className="meme--image" />
+    </div>
   );
 }
